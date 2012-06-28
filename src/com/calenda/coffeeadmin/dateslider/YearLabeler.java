864ce49e87a976d31bfa4a2828ea9d3085b478b9
calenda <calenda.software@gmail.com>
2012-06-28 14:04:01 -0500

    - DateSlider classes

---
+++ src/com/calenda/coffeeadmin/dateslider/YearLabeler.java
@@ -0,0 +1,25 @@
+package com.calenda.coffeeadmin.dateslider;
+
+import java.util.Calendar;
+
+/**
+ * A Labeler that displays months
+ */
+public class YearLabeler extends Labeler {
+    private final String mFormatString;
+
+    public YearLabeler(String formatString) {
+        super(200, 60);
+        mFormatString = formatString;
+    }
+
+    @Override
+    public TimeObject add(long time, int val) {
+        return timeObjectfromCalendar(Util.addYears(time, val));
+    }
+
+    @Override
+    protected TimeObject timeObjectfromCalendar(Calendar c) {
+        return Util.getYear(c, mFormatString);
+    }
+}
